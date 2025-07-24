9456ccd15831448b95cae6d5ea9ecce17961359c
Gowtham <gowthamnag04@gmail.com>
2025-07-24 23:57:29 +0530

    front in transition to react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+
+const Card  = ({tittle,rating,genre,actors}) =>{
+  return(
+        < div className='card'>
+          <h3>{tittle}</h3>
+          <h4>Rating : {rating}</h4>
+          <h4>Genre : {genre}</h4>
+        </div>
+        )
+}
+
+const app = ()=>{
+  return(
+    <div className='card-container'>
+      <Card tittle="Lion King1" rating = "4" genre="story"  />
+      <Card tittle="Star wars" rating = "5" genre="action"  />
+      <Card tittle="Mufasa" rating = "3" genre="story"  />
+      <Card tittle="Game of Thrones" rating = "4.7" genre="history" />
+
+    </div>
+  )
+}
+
+export default app
